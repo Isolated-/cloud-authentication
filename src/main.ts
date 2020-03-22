63aebbb3302c2c7a0462836693971426477f1c63
Michael Palmer <michaelpalmer53@gmail.com>
2020-03-22 03:08:09 +0000

    adds security + compression middleware

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,27 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { MonitorService, MonitorInterceptor } from '@monitor/monitor';
 
+import * as helmet from 'helmet';
+import * as csurf from 'csurf';
+import * as rateLimit from 'express-rate-limit';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const service = app.get<MonitorService>(MonitorService);
   app.useGlobalInterceptors(new MonitorInterceptor(service));
+
+  // security + compression + rate limit middleware
+  app.use(helmet());
+  app.use(csurf());
+  app.use(
+    rateLimit({
+      windowMs: 1 * 60 * 1000,
+      max: 5, // one request per second
+    }),
+  );
+
+  app.enableCors();
 
   await app.listen(3000);
 }
